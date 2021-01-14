3cabcbf67bd311c2167b9821ec46f4da7b1cd1a5
Melissa Faulner <melissa.faulner@gmail.com>
2021-01-14 18:47:14 -0500

    commit tree 73f1a2acc7244f1d6bed96619a40d81d1a542f81

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,19 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @new_student = Student.new
   end
 
   def create
+    @student = Student.create(student_params(:first_name, :last_name))
+
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def student_params(*arg)
+    params.require(:student).permit(*arg)
   end
 
 end
